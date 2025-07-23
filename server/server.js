6398c9ff24776e9e55c4dace78fd61a2531b0304
Vansh <143195512+Vansh-Raikwar@users.noreply.github.com>
2025-07-23 23:35:57 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -12,7 +12,12 @@ const app = express();
 // Connect to database
 connectDB();
 
-
+app.use(cors({
+  origin: ['http://localhost:5173', 'http://localhost:3000', 'https://your-frontend-domain.com'],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'token']
+}));
 app.use(express.json())
 
 
