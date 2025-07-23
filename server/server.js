4aaddad9451949e8b3c966ecbb38311ab911b3c2
Vansh-Raikwar <vanshraikwar880@gmail.com>
2025-07-23 22:57:14 +0530

    update server.js

--- server/server.js
+++ server/server.js
@@ -12,13 +12,7 @@ const app = express();
 // Connect to database
 connectDB();
 
-// Intialize Middlewares
-app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000'], // Add your frontend URLs
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'token']
-}))
+
 app.use(express.json())
 
 
