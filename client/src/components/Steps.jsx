4054d1d5697ec887751b49cd50d322d7be9caad9
Vansh-Raikwar <vanshraikwar880@gmail.com>
2025-07-25 09:54:52 +0530

    update some lines

--- client/src/components/Steps.jsx
+++ client/src/components/Steps.jsx
@@ -13,7 +13,7 @@ const Steps = () => {
                     <img className='max-w-9' src={assets.upload_icon} alt="" />
                     <div>
                         <p className='text-xl font-medium'>Upload image</p>
-                        <p className='text-sm text-neutral-500 mt-1'>This is a demo text, will replace it later. <br /> This is a demo..</p>
+                        <p className='text-sm text-neutral-500 mt-1'>Easily upload images from your device in seconds <br /> Supports all major formats</p>
                     </div>
                 </div>
 
@@ -21,7 +21,7 @@ const Steps = () => {
                     <img className='max-w-9' src={assets.remove_bg_icon} alt="" />
                     <div>
                         <p className='text-xl font-medium'>Remove background</p>
-                        <p className='text-sm text-neutral-500 mt-1'>This is a demo text, will replace it later. <br /> This is a demo..</p>
+                        <p className='text-sm text-neutral-500 mt-1'>Quickly remove image backgrounds with high accuracy. <br /> Get clean, transparent results in seconds.</p>
                     </div>
                 </div>
 
@@ -29,7 +29,7 @@ const Steps = () => {
                     <img className='max-w-9' src={assets.download_icon} alt="" />
                     <div>
                         <p className='text-xl font-medium'>Download image</p>
-                        <p className='text-sm text-neutral-500 mt-1'>This is a demo text, will replace it later. <br /> This is a demo..</p>
+                        <p className='text-sm text-neutral-500 mt-1'>TDownload your edited images instantly in high quality. <br /> TNo watermark, completely free.</p>
                     </div>
                 </div>
 
